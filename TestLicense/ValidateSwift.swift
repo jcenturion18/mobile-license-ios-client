68af6b7cc2f625ac942ae34c92095eeb4ca76466
Julian Centurion <julian@avenza.com>
2024-10-04 11:41:03 -0400

    Simple project

--- TestLicense/ValidateSwift.swift
+++ TestLicense/ValidateSwift.swift
@@ -5,4 +5,36 @@
 //  Created by Julian Centurion on 1/10/24.
 //
 
-import Foundation
+import LicenseManager
+
+class ValidateSwift {
+
+    func doTheHarlemShake() {
+
+        let headers = ["User-Agent": "iOS"]
+
+        let request = RequestImp(device: "iOS", headers: headers, url: "http://127.0.0.1:3000/")
+        ValidateKt.validate(request: request) { response, _ in
+            if let error {
+
+            }
+
+            if let response {
+                print(response)
+            }
+        }
+
+    }
+}
+
+class RequestImp: Request {
+    let device: String
+    let headers: [String: String]
+    let url: String
+
+    init(device: String, headers: [String: String], url: String) {
+        self.device = device
+        self.headers = headers
+        self.url = url
+    }
+}
